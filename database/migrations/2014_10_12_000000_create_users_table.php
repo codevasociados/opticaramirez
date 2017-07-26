1fa5263ac2e79982bc02032b6acba511165fc0d3
lq150415 <lq150415@gmail.com>
2017-07-26 19:33:02 -0400

    Migraciones y configuraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,11 @@
-<?php
 
+<?php
+ /** 
+    Developer: Luis Quisbert
+    Date: 26/07/17
+    Hour: 18:44
+    Comment: Migration table users
+ */
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,9 +21,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('nam_user');    //Nombre de usuario
+            $table->string('lpa_user');    //Apellido paterno
+            $table->string('lma_user');    //Apellido materno
+            $table->string('ci_user');     //Carnet de identidad usuario
+            $table->text('add_user');    //Direccion del usuario
+            $table->integer('pho_user',11);//Telefono de usuario
+            $table->string('nic_user')->unique();// Nick del usuario
+            $table->string('password');    //Contraseña de usuario
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2017_07_26_185139_create_perfil_table.php
@@ -0,0 +1,42 @@
+<?php
+
+/** 
+    Developer: Luis Quisbert
+    Date: 26/07/17
+    Hour: 18:44
+    Comment: Migration table perfil
+ */
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerfilTable extends Migration
+{
+   /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('lvl_pro');
+            $table->datetime('ini_pro');
+            $table->datetime('end_pro');
+            $table->integer('id_user')->unsigned();            
+            $table->foreign('id_user')->references('id')->on('users+');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
