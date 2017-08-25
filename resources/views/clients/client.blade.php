35966ee4af544e5d36bc08d5c528b3d9bd1e8f5f
lq150415 <lq150415@gmail.com>
2017-08-25 01:51:40 -0400

    View client

--- resources/views/clients/client.blade.php
+++ resources/views/clients/client.blade.php
@@ -2,4 +2,15 @@
 
 @section('title')
 	Clientes - Optica ramirez
-@endsection 
+@endsection
+
+@section('content')
+<div class="content-fluid">
+	<div class="col-md-6">
+
+	</div>
+	<div class="col-md-5">
+
+	</div>
+</div>
+@endsection
